c9ade7d1d2a2c1af98ece843697000cc4c00a9af
ostrygun <oleg_strygun@intuit.com>
2022-12-19 20:58:36 -0800

    JSON POST added

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -12,6 +12,10 @@ import com.github.plokhotnyuk.jsoniter_scala.macros._
 import com.github.plokhotnyuk.jsoniter_scala.core._
 import fs2.{Stream, Chunk}
 
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
+import io.quartz.MyLogger._
+
 //To re-generate slef-signed cert use.
 //keytool -genkey -keyalg RSA -alias selfsigned -keystore keystore.jks -storepass password -validity 360 -keysize 2048
 //in your browser: https://localhost:8443  ( click vist website, you need to accept slef-cigned cert first time )
@@ -23,17 +27,32 @@ object Main extends IOApp {
 
   given codec: JsonValueCodec[User] = JsonCodecMaker.make
 
-  val R: HttpRouteIO = { case GET -> Root / "json" =>
-    for {
-      json <- IO(
-        writeToArray(
-          User(name = "John", devices = Seq(Device(id = 2, model = "iPhone X")))
+  val R: HttpRouteIO = {
+    //////////////////////////////
+    case GET -> Root / "json" =>
+      for {
+        json <- IO(
+          writeToArray(
+            User(
+              name = "John",
+              devices = Seq(Device(id = 2, model = "iPhone X"))
+            )
+          )
         )
-      )
-    } yield (Response
-      .Ok()
-      .asStream(Stream.chunk(Chunk.array(json)))
-      .contentType(JSON))
+      } yield (Response
+        .Ok()
+        .asStream(Stream.chunk(Chunk.array(json)))
+        .contentType(JSON))
+
+    ///////////////////////////////    
+    // Body POST example: { "name" : "John", "devices" : [{"id":1,"model":"HTC One X"}] }
+    case req @ POST -> Root / "user" =>
+      for {
+        payload <- req.body
+        user <- IO(readFromArray(payload))
+        _ <- Logger[IO].info( "json-template-gh2: user: " +  user.toString())
+
+      } yield (Response.Ok().asText(user.name + " - accepted"))
   }
 
   def run(args: List[String]): IO[ExitCode] =
