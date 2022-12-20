168d8bc4951c75fe08ce45bad5b1361eba179b28
ostrygun <oleg_strygun@intuit.com>
2022-12-19 16:28:18 -0800

    init

---
+++ build.sbt
@@ -0,0 +1,11 @@
+ThisBuild / organization := "com.example"
+ThisBuild / scalaVersion := "3.2.1"
+
+lazy val root = (project in file(".")).settings(
+  name := "json-template-qh2",
+  libraryDependencies ++= Seq(
+     "io.github.ollls" %% "quartz-h2" % "0.2.0",
+     "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core"   % "2.19.1",
+     "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % "2.19.1" % "compile-internal"
+  )
+)
---
+++ src/main/scala/com/example/Main.scala
@@ -0,0 +1,46 @@
+package com.example
+
+import cats.effect.{IO, IOApp, ExitCode}
+import io.quartz.QuartzH2Server
+import io.quartz.http2.routes.{HttpRouteIO, Routes}
+import io.quartz.http2.model.{Headers, Method, ContentType, Request, Response}
+import io.quartz.http2._
+import io.quartz.http2.model.Method._
+import io.quartz.http2.model.ContentType.JSON
+
+import com.github.plokhotnyuk.jsoniter_scala.macros._
+import com.github.plokhotnyuk.jsoniter_scala.core._
+import fs2.{Stream, Chunk}
+
+//To re-generate slef-signed cert use.
+//keytool -genkey -keyalg RSA -alias selfsigned -keystore keystore.jks -storepass password -validity 360 -keysize 2048
+//in your browser: https://localhost:8443  ( click vist website, you need to accept slef-cigned cert first time )
+
+case class Device(id: Int, model: String)
+case class User(name: String, devices: Seq[Device])
+
+object Main extends IOApp {
+
+  given codec: JsonValueCodec[User] = JsonCodecMaker.make
+
+  val R: HttpRouteIO = { case GET -> Root / "json" =>
+    for {
+      json <- IO(
+        writeToArray(
+          User(name = "John", devices = Seq(Device(id = 2, model = "iPhone X")))
+        )
+      )
+    } yield (Response
+      .Ok()
+      .asStream(Stream.chunk(Chunk.array(json)))
+      .contentType(JSON))
+  }
+
+  def run(args: List[String]): IO[ExitCode] =
+    for {
+      ctx <- QuartzH2Server.buildSSLContext("TLS", "keystore.jks", "password")
+      exitCode <- new QuartzH2Server("localhost", 8443, 16000, ctx)
+        .startIO(R, sync = false)
+
+    } yield (exitCode)
+}
